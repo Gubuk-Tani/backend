247485b30157fa598f677ee04c41d88de74da173
Sahid Anwar <hay.sahid@gmail.com>
2023-05-22 08:43:12 +0700

    add user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,6 +34,10 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
             'profile_photo_path' => null,
             'current_team_id' => null,
+            
+            'username' => $this->faker->userName(),
+            'city' => $this->faker->city(),
+            'avatar' => null,
         ];
     }
 
@@ -54,14 +58,14 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam(callable $callback = null): static
     {
-        if (! Features::hasTeamFeatures()) {
+        if (!Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
                 ->state(fn (array $attributes, User $user) => [
-                    'name' => $user->name.'\'s Team',
+                    'name' => $user->name . '\'s Team',
                     'user_id' => $user->id,
                     'personal_team' => true,
                 ])
