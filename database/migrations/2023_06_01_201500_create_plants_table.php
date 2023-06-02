ef3369f62992bb7c411d2654d4939cf4932b8477
Sahid Anwar <hay.sahid@gmail.com>
2023-06-02 09:59:36 +0700

    fix: set nullable to image column

--- database/migrations/2023_06_01_201500_create_plants_table.php
+++ database/migrations/2023_06_01_201500_create_plants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->enum('status', ['Active', 'Inactive']);
 
             $table->softDeletes();
