8b98ca7de9f8feadc56e5732c56ed750989f5317
Sahid Anwar <hay.sahid@gmail.com>
2023-06-02 09:03:12 +0700

    feat: machine learning integration

---
+++ app/Models/Detection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Detection extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'image',
+        'result',
+        'plant_id',
+        'user_id',
+    ];
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_06_01_202532_create_detections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detections', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('image');
+            $table->string('result');
+            $table->bigInteger('plant_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('plant_id')->references('id')->on('plants')->onUpdate('cascade')->onDelete('no action');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('no action');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detections');
+    }
+};
